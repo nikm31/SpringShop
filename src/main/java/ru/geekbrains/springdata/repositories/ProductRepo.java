a64c57ec341ce610b16a349b3c504fbd891a2d85
Николай <firewather@gmail.com>
2022-03-02 10:21:44 +0300

    with SOAP

--- src/main/java/ru/geekbrains/springdata/repositories/ProductRepo.java
+++ src/main/java/ru/geekbrains/springdata/repositories/ProductRepo.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import ru.geekbrains.springdata.entity.Product;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +16,6 @@ public interface ProductRepo extends PagingAndSortingRepository<Product, Long>, 
     Double findMaxPrice();
 
     Optional<Product> findByTitle(String title);
+
+    List<Product> findAll();
 }
